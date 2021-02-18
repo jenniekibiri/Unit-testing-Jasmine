785bd2f163ea7662826ebe2d2d6a46e968919d81
jenniekibiri <josphatje@gmail.com>
2021-02-18 18:14:28 +0300

    learn spies

--- Todo.js
+++ Todo.js
@@ -18,3 +18,19 @@ function stringConv(inputString){
          return false;  
    },  
 };
+
+var Person = function() {}; 
+
+Person.prototype.sayHelloWorld = function(dict) { 
+   return dict.hello() + " " + dict.world(); 
+}; 
+
+var Dictionary = function() {}; 
+
+Dictionary.prototype.hello = function() { 
+   return "hello"; 
+}; 
+
+Dictionary.prototype.world = function() { 
+   return "world"; 
+}; 
--- test/TodoSpec.js
+++ test/TodoSpec.js
@@ -69,8 +69,30 @@ describe('This custom matcher example', function() {
 
 
 
+describe("Different Methods of Expect Block",function () { 
+   it("Example of  toBeCloseTo()", function () { 
+      expect(12.34).toBeCloseTo(12.3,1);
+   });
+}); 
 
 
-
+//spies
+describe("Example Of jasmine Spy using spyOn()", function() { 
+  
+   it('uses the dictionary to say "hello world"', function() { 
+      var dictionary = new Dictionary; 
+      var person = new Person; 
+		
+      spyOn(dictionary, "hello");  // replace hello function with a spy 
+      spyOn(dictionary, "world");  // replace world function with another spy 
+		
+      person.sayHelloWorld(dictionary);
+      expect(dictionary.hello).toHaveBeenCalled();  
+      // not possible without first spy 
+  
+      expect(dictionary.world).toHaveBeenCalled();  
+      // not possible withoutsecond spy 
+   }); 
+});
 
 
