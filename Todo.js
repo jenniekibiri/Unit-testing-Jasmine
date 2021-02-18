5efd5ad878c72a25ebe86dd89025266865d70e28
jenniekibiri <josphatje@gmail.com>
2021-02-18 16:08:29 +0300

    check string conversion

--- Todo.js
+++ Todo.js
@@ -4,3 +4,9 @@ return "welcome ann"
 function addition(num1,num2){
     return num1+num2
 }
+function  multiply(num1,num2){
+    return num1*num2
+}
+function stringConv(inputString){
+    return inputString.toUpperCase()
+}
--- test/TodoSpec.js
+++ test/TodoSpec.js
@@ -10,5 +10,15 @@ describe('arithmetic test',function(){
     it("addition it should return the summation of two numbers",function () {
         expect(addition(2,2)).toEqual(4)
     })
+it("it should multiply two number",function(){
+    expect(multiply(9,9)).toBe(81)
+})
+})
+describe('String operations',function(){
+    it("should convert a string to uppercase",function () {
+        console.log(stringConv('hello'))
+
+        expect(stringConv('hello my name is jenny')).toEqual('HELLO MY NAME IS JENNY')
+    })
 
 })
